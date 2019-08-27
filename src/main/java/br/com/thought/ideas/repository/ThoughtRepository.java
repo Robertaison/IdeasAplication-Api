a28769a001f606d64baa931fc816012426dd7428
Robertson Souza <roberts.sbeer@gmail.com>
2019-08-27 09:36:44 -0300

    Ordenação por data mais recente de post

--- src/main/java/br/com/thought/ideas/repository/ThoughtRepository.java
+++ src/main/java/br/com/thought/ideas/repository/ThoughtRepository.java
@@ -5,6 +5,9 @@ import br.com.thought.ideas.domain.ThoughtEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ThoughtRepository extends JpaRepository<ThoughtEntity, Long> {
+    List<ThoughtEntity> findAllByOrderByDataDesc();
 }
